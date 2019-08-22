6a781c50eb5c29e7bac8d17013733ebc18f08c27
manideeppasuladi <44994267+manideeppasuladi@users.noreply.github.com>
2019-08-22 19:23:08 +0530

    'Done'

--- Data/application/views/team.php
+++ Data/application/views/team.php
@@ -1,5 +1,9 @@
  <!-- Organising Body -->
-
+<style>
+.body .card{
+  height:25em; 
+  padding-bottom: 1em;
+}</style>
  <div class="container pt-5 pb-5" id="organisingBody">
         
         <div class="container">
@@ -23,9 +27,10 @@
            
             <h2 class="h1-responsive font-weight-bold text-center mb-5 pt-5 wow fadeInUp">ORGANISING BODY</h2>
             
-            <div class="container">
+            <div class="container  body">
               <div class="row justify-content-center align-items-center text-center">
-                <div class="col-sm-12 col-md-6 col-lg-4 mb-2 hvr-shadow-radial" >
+                
+                <div class="col-sm-12 col-md-3 col-lg-3 mb-2 hvr-shadow-radial" >
                   <div class="card ">
                     <img class="card-img-top img-thumbnail rounded-circle  "   src="<?php echo base_url('assets/images/1.jpeg');?>" alt="Chairman">
                     <div class="card-body">
@@ -35,7 +40,7 @@
                   </div>
                 </div>
         
-                <div class="col-sm-12 col-md-6 col-lg-4 mb-2 hvr-shadow-radial" >
+                <div class="col-sm-12 col-md-3 col-lg-3 mb-2 hvr-shadow-radial" >
                   <div class="card ">
                     <img class="card-img-top img-thumbnail rounded-circle  "  src="<?php echo base_url('assets/images/2.jpeg');?>" alt="Secretary">
                     <div class="card-body">
@@ -44,16 +49,7 @@
                     </div>
                   </div>
                 </div>
-        
-        
-              </div>
-            </div>
-            <!--end of cheif patron-->
-            <hr>
-            <!--patron and gen chair-->
-            <div class="container">
-              <div class="row justify-content-center align-items-center text-center">
-                <div class="col-sm-12 col-md-6 col-lg-4 mb-2 hvr-shadow-radial" >
+                <div class="col-sm-12 col-md-3 col-lg-3 mb-2 hvr-shadow-radial" >
                  <div class="card">
                   <img class="card-img-top img-thumbnail rounded-circle  "   src="<?php echo base_url('assets/images/3.jpeg');?>" alt="Director">
                   <div class="card-body">
@@ -63,8 +59,8 @@
                   
                 </div>
               </div>
-        
-           <div class="col-sm-12 col-md-6 col-lg-4 mb-2 hvr-shadow-radial" >
+
+              <div class="col-sm-12 col-md-3 col-lg-3 mb-2 hvr-shadow-radial" >
                  <div class="card">
                   <img class="card-img-top img-thumbnail rounded-circle  "   src="<?php echo base_url('assets/images/6.jpeg');?>" alt="Director">
                   <div class="card-body">
@@ -74,18 +70,8 @@
                   
                 </div>
               </div>
-        
-            </div>
-          </div>
-        
-          <!--end of patron and gen chair-->
-          <hr>
-          <!--Program chair and convenor-->
-        
-        
-          <div class="container">
-            <div class="row justify-content-center align-items-center text-center ">
-              <div class="col-sm-12 col-md-6 col-lg-4 mb-2 hvr-shadow-radial" >
+
+              <div class="col-sm-12 col-md-3 col-lg-3 mb-2 hvr-shadow-radial" >
                <div class="card">
                 <img class="card-img-top img-thumbnail rounded-circle  "   src="<?php echo base_url('assets/images/4.jpeg');?>" alt="Program_chair">
                 <div class="card-body">
@@ -95,7 +81,7 @@
               </div>
             </div>
         
-            <div class="col-sm-12 col-md-6 col-lg-4 mb-2 hvr-shadow-radial" >
+            <div class="col-sm-12 col-md-3 col-lg-3 mb-2 hvr-shadow-radial" >
              <div class="card">
               <img class="card-img-top img-thumbnail rounded-circle   "   src="<?php echo base_url('assets/images/5.jpeg');?>" alt="Program_chair">
               <div class="card-body">
@@ -104,42 +90,38 @@
               </div>
             </div>
           </div>
-        
-          
-        </div>
-        </div>
-        
-        <hr>
-        <!--End of Program chair and convenor -->
+              
+              </div>
+              
+            </div>
+           
+            <hr>
         <div class="container">
-          <center>
+         
+        <div class="row justify-content-center align-items-center text-center">
+        <div class="col-sm-12 col-md-6 col-lg-4 mb-2 hvr-shadow-radial" >
+              <h2 class="h3-responsive font-weight-bold text-center mb-5 pt-5 wow fadeInUp">INDUSTRY ASSOCIATION</h2>
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-4 mb-2 hvr-shadow-radial" >
+              <h2 class="h3-responsive font-weight-bold text-center mb-5 pt-5 wow fadeInUp">INDUSTRY EXPERT</h2>
+        </div>
+        </div>
             
-            <h2 class="h1-responsive font-weight-bold text-center mb-5 pt-5 wow fadeInUp">INDUSTRY ASSOCIATION</h2>
             <div class="row justify-content-center align-items-center text-center">
               <div class="col-sm-12 col-md-6 col-lg-4 mb-2 hvr-shadow-radial" >
+              
                <div class="card">
                 <img class="card-img-top img-thumbnail  "   src="<?php echo base_url('assets/images/Qlik.jpg');?>" alt="Program_chair">
                 <div class="card-body">
                   <h4 class="card-title" style="color: black;">Qlik Pvt. Ltd.</h4>
                   <h6 class="card-subtitle mb-2 text-body"></h6>
                 </div>
+                
               </div>
+              
             </div>
-        
-        
-          </div>
-        </div>
-        <!--steering committee-->
-        
-        
-        <hr>
-        
-        <div class="container">
-          <center>
+            <div class="col-sm-12 col-md-6 col-lg-4 mb-2 hvr-shadow-radial " >
             
-            <h2 class="h1-responsive font-weight-bold text-center mb-5 pt-5 wow fadeInUp">INDUSTRY EXPERT</h2>
-            <div class="row justify-content-center align-items-center text-center">
-              <div class="col-sm-12 col-md-6 col-lg-4 mb-2 hvr-shadow-radial" >
                <div class="card">
                 <img class="card-img-top img-thumbnail rounded-circle  "   src="<?php echo base_url('assets/images/IndustryExpert.JPG');?>" alt="Program_chair">
                 <div class="card-body">
@@ -149,12 +131,12 @@
               </div>
             </div>
         
-        
-        
-        
           </div>
+
         </div>
+        <!--steering committee-->
         
+       
         
         <!--end of steering committee-->
         <hr>
